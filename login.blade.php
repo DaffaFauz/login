27811fc87b0ce456aed23be4361e04c45a8ed69f
Daffa Fauzul Hakim <94423743+DaffaFauz@users.noreply.github.com>
2022-03-15 17:54:33 +0700

    Add files via upload

---
+++ login.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>SPP Ujikom | Login</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="css/dist.css" rel="stylesheet">
+
+    
+    <!-- Custom styles for this template -->
+    <link href="/css/login.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<main class="form-signin">
+  <form action="/login" method="POST">
+    @csrf
+    <h1 class="mb-3 text-white text-center">Login</h1>
+
+    <div class="form-floating mb-3">
+      <input type="text" class="form-control" id="floatingInput" placeholder="Username or Email">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+        <input type="checkbox" value="remember-me">
+        <label class="text-white">Remember me</label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+  </form>
+</main>
+
+
+    
+  </body>
+</html>
